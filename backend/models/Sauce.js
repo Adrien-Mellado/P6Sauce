71248180e72693fe583aae73fd6d4361f18a2d08
Adrien-Mellado <adrienmellado@outlook.fr>
2021-09-23 11:05:14 +0200

    mise en place des models

---
+++ backend/models/Sauce.js
@@ -0,0 +1,17 @@
+const mongoose = require ('mongoose');
+
+const SauceSchema = mongoose.Schema ({
+    userId: { type : String , required: false},
+    name : {type :  String , required: false },
+    manufacturer : { type :  String , required: false },
+    description : { type : String , required: false },
+    mainPepper : { type :  String , required: false },
+    imageUrl : { type : String , required: true},
+    heat : { type : Number , required: false },
+    likes :  { type : Number , required: false , default:0},
+    dislikes : { type : Number , required: false , default:0},
+    usersLiked : { type : Array , required: false , default:{} },
+    usersDisliked : { type : Array , required: false , default:{}  },
+});
+
+module.exports = mongoose.model ('Sauce', SauceSchema) ;
