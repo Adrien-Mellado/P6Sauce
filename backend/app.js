e8d73277a6700dc9d7aba32f31192b9acbf8711d
Adrien-Mellado <adrienmellado@outlook.fr>
2021-09-23 11:06:37 +0200

    mise en place du backend

---
+++ backend/app.js
@@ -0,0 +1,64 @@
+/* mise en place de express */
+const express = require('express');
+const bodyParser = require('body-parser');
+
+
+/* importez mongoose */
+const mongoose = require('mongoose');
+
+/*importer dotenv*/
+const dotenv = require("dotenv");
+dotenv.config();
+
+const app = express();
+const path = require ('path');
+
+
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+  next();
+});
+
+
+app.use(bodyParser.json());
+
+/* connection a mongoose */ 
+mongoose.connect(`mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@cluster0.fmg4k.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`,
+  { useNewUrlParser: true,
+    useUnifiedTopology: true })
+  .then(() => console.log('Connexion à MongoDB réussie !'))
+  .catch(() => console.log('Connexion à MongoDB échouée !'));
+
+  /*
+app.use ((req,res )=>{
+ 
+    res.json ({ message: 'votre requete a bien ete recue'});
+    
+})*/
+
+app.use('/images', express.static(path.join(__dirname, 'images')));
+
+
+/**
+ * instanciate routers
+ */
+const userRoutes = require('./routes/user');
+const sauceRoutes = require('./routes/sauce');
+
+/**
+ * set up routes
+ */
+app.use('/api/sauces',  sauceRoutes);
+app.use('/api/auth',  userRoutes);
+
+
+
+
+
+
+
+
+/* exporter express */ 
+module.exports = app ;
---
+++ backend/like_dislike.js
@@ -0,0 +1,11 @@
+let btn_like = document.querySelectorAll(".like-buttons");
+
+
+btn_like.addEventListener ("click", (event) =>{
+
+   
+    btn_like = 1
+   
+
+
+})
