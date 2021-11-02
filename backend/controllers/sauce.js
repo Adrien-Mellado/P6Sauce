a742db61530d7b8da7cbfd50e471020693302bb8
Adrien-Mellado <adrienmellado@outlook.fr>
2021-11-02 17:50:37 +0100

    modification supressionSauce

--- backend/controllers/sauce.js
+++ backend/controllers/sauce.js
@@ -1,4 +1,5 @@
 const Sauce = require("../models/Sauce");
+const fs = require ('fs');
 
 // Ajout d'une sauce //
 exports.CreationSauce = (req, res, next) => {
@@ -39,10 +40,17 @@ exports.ModificationSauce = (req, res, next) => {
 
 // supression d'une sauce //
 exports.SupressionSauce = (req, res, next) => {
-  Sauce.deleteOne({ _id: req.params.id })
-    .then(() => res.status(200).json({ message: "Objet supprimé !" }))
-    .catch((error) => res.status(400).json({ erreur: error }));
-};
+  Sauce.findOne({ _id: req.params.id })
+  .then(sauce => {
+      const filename = sauce.imageUrl.split('/images/')[1];
+      fs.unlink(`images/${filename}`, () => {
+          Sauce.deleteOne({ _id: req.params.id })
+              .then(() => res.status(200).json({ message: 'Objet supprimé !' }))
+              .catch(error => res.status(400).json({ error }));
+      });
+  })
+  .catch(error => res.status(500).json({ error }));
+}
 
 // recuperation d'une seule sauce //
 exports.Recuperation_une_Sauce = (req, res, next) => {
